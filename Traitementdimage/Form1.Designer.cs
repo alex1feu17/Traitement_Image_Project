2a7af85b067c276fe2d153363942e715c6a5ca85
fatcoco1 <78938041+fatcoco1@users.noreply.github.com>
2023-02-13 17:27:15 -0500

    contour and threshold
    le contouring des objets

--- Traitementdimage/Form1.Designer.cs
+++ Traitementdimage/Form1.Designer.cs
@@ -33,9 +33,10 @@ namespace Traitementdimage
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1148, 739);
+            this.ClientSize = new System.Drawing.Size(1004, 554);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Traitementdimage/Program.cs
+++ Traitementdimage/Program.cs
@@ -1,10 +1,10 @@
+using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Security;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Traitementdimage
@@ -25,20 +25,80 @@ namespace Traitementdimage
         }
         public partial class Form1 : Form
         {
+            private NumericUpDown minCanny,maxCanny,minThresh,maxThresh;
             private PictureBox pictureBox;
             private Button buttonLoadImage;
             private Button buttonGetImage;
+            private Button buttonProcessImage;
             private OpenFileDialog openFileDialog1;
             private TextBox textBox1;
-            private string filePath = string.Empty;
+            private Label labelmincan,labelmaxcan,labelminthresh,labelmaxthresh;
+              
 
+            private string filePath = string.Empty;
+            private Dictionary<string, Image<Bgr,byte>> images;
             public Form1()
-            {           
+            {
+                images = new Dictionary<string, Image<Bgr, byte>>();
+                labelmincan = new Label {
+                    Size = new Size(130, 30),
+                    Location = new Point(400, 20),
+                    Text ="minimum Canny :"};
+                labelmaxcan = new Label {
+                    Size = new Size(130, 30),
+                    Location = new Point(400, 55),
+                    Text = "maximum Canny :" };
+                labelminthresh = new Label {
+                    Size = new Size(130, 30),
+                    Location = new Point(400, 90),
+                    Text = "minimum threshold :" };
+                labelmaxthresh = new Label {
+                    Size = new Size(130, 30),
+                    Location = new Point(400, 120),
+                    Text = "maximum threshold :" };
+                minCanny = new NumericUpDown {
+                    Size = new Size(100, 30),
+                    Location = new Point(560, 20),
+                    Maximum = 255,
+                    Minimum = 0,
+                    Value = 100,
+                   
+                };
+                maxCanny = new NumericUpDown
+                {
+                    Size = new Size(100, 30),
+                    Location = new Point(560, 55),
+                    Maximum = 255,
+                    Minimum = 0,
+                    Value = 255,
+                    
+                    
+                    
+                };
+                minThresh = new NumericUpDown
+                {
+                    Size = new Size(100, 30),
+                    Location = new Point(560, 90),
+                    Maximum = 255,
+                    Minimum = 0,
+                    Value = 10,
+                    
+                };
+                maxThresh = new NumericUpDown
+                {
+                    Size = new Size(100, 30),
+                    Location = new Point(560, 120),
+                    Maximum = 255,
+                    Minimum = 0,
+                    Value = 2,
+                    
+                };
                 
+
                 buttonLoadImage = new Button {
                     Size =  new Size(100, 30),
                     Location = new Point(10, 10),
-                    Text = "Load Image",
+                    Text = "Reload Image"
                 };
                
                 buttonGetImage = new Button
@@ -47,16 +107,31 @@ namespace Traitementdimage
                     Location = new Point(130, 10),
                     Text = "Select file"
                 };
+                buttonProcessImage = new Button
+                {
+                    Size = new Size(100, 30),
+                    Location = new Point(300, 10),
+                    Text = "Process Image",
+                };
                 buttonLoadImage.Click += new EventHandler(this.Button_Click);
                 buttonGetImage.Click += new EventHandler(SelectButton_Click);
+                buttonProcessImage.Click += new EventHandler(ProcessButton_Click);
 
                 pictureBox = new PictureBox {
                     Location = new Point(10, 150),
                     Size = new Size(1000, 1000)
                 };
-
+                this.Controls.Add(this.minCanny);
+                this.Controls.Add(this.maxCanny);
+                this.Controls.Add(this.minThresh);
+                this.Controls.Add(this.maxThresh);
                 this.Controls.Add(this.pictureBox);
                 this.Controls.Add(this.buttonLoadImage);
+                this.Controls.Add(this.buttonProcessImage);
+                this.Controls.Add(labelmincan);
+                this.Controls.Add(labelmaxcan);
+                this.Controls.Add(labelminthresh);
+                this.Controls.Add(labelmaxthresh);
 
                 openFileDialog1 = new OpenFileDialog();
         
@@ -67,7 +142,7 @@ namespace Traitementdimage
                     Multiline = true,
                     ScrollBars = ScrollBars.Vertical
                 };
-                ClientSize = new Size(330, 360);
+                ClientSize = new Size(930, 960);
                 Controls.Add(buttonGetImage);
                 Controls.Add(textBox1);
             }
@@ -85,6 +160,17 @@ namespace Traitementdimage
                         var sr = new StreamReader(openFileDialog1.FileName);
                         filePath = openFileDialog1.FileName;
                         textBox1.Text = filePath;
+                        string imagePath = textBox1.Text;
+                        var img = new Image<Bgr, byte>(imagePath);
+                        img = img.Resize(800, 500, Emgu.CV.CvEnum.Inter.Linear);
+                        this.pictureBox.Image = img.ToBitmap();
+                        if (images.ContainsKey("input"))
+                        {
+                            images.Remove("input");
+                        }
+
+                        images.Add("input", img);
+                        Console.WriteLine("Image loaded successfully.");
                     }
                     catch (SecurityException ex)
                     {
@@ -96,10 +182,58 @@ namespace Traitementdimage
             private void Button_Click(object sender, EventArgs e)
             {
                 string imagePath = textBox1.Text;
+                var img = new Image<Bgr, byte>(imagePath);
+                img = img.Resize(800, 500, Emgu.CV.CvEnum.Inter.Linear);
+                this.pictureBox.Image = img.ToBitmap();
+                if (images.ContainsKey("input"))
+                {
+                    images.Remove("input");
+                }
 
-                Bitmap image = new Bitmap(imagePath);
-                this.pictureBox.Image = image;
+                images.Add("input", img);
                 Console.WriteLine("Image loaded successfully.");
+            }
+            private void ProcessButton_Click(object sender, EventArgs e)
+            {
+                try {
+                    if (images.ContainsKey("input"))
+                    {
+                        var img = images["input"];
+
+                        Mat thresholdpic = new Mat();
+                        Mat edgespic =new Mat();
+                        Mat bluredimg = new Mat();
+                        Mat countourImg =new Mat();
+
+                        CvInvoke.MedianBlur(img,bluredimg, 5);
+                        CvInvoke.CvtColor(bluredimg,countourImg,Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                        //CvInvoke.Threshold(img, thresholdpic, 120, 155, Emgu.CV.CvEnum.ThresholdType.Binary);
+                        
+
+                        int minthresh = (int)minThresh.Value;
+                        double maxcthresh = (double)maxThresh.Value;
+                        CvInvoke.AdaptiveThreshold(countourImg, thresholdpic,255,Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC, Emgu.CV.CvEnum.ThresholdType.Binary,minthresh,maxcthresh);
+
+                        double mincanny = (double)minCanny.Value;
+                        double maxcanny = (double)maxCanny.Value;
+                        CvInvoke.Canny(thresholdpic, edgespic, mincanny, maxcanny);
+                        
+                        Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+                        
+                          CvInvoke.FindContours(edgespic,
+                                              contours,thresholdpic,
+                                              Emgu.CV.CvEnum.RetrType.External,
+                                              Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+                        CvInvoke.DrawContours(img, contours,-1,new MCvScalar(255,0,0));
+                        this.pictureBox.Image = img.ToBitmap();
+
+                    }
+                    else {
+                        throw new Exception("Choose an image first");
+                    }
+                       
+                
+                } catch { }
             }
         }
     }
